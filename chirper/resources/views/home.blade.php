0519a23318d2a9da0028112fa6155490728f324b
Sofroniebruh <sofronie.albu1@student.nhlstenden.com>
2026-01-08 20:24:02 +0100

    feature: add model

--- chirper/resources/views/home.blade.php
+++ chirper/resources/views/home.blade.php
@@ -3,16 +3,19 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
-        @foreach($chirps as $chirp)
+        @forelse($chirps as $chirp)
             <div class="card bg-base-100 shadow mt-8">
                 <div class="card-body">
                     <div>
-                        <div class="font-bold">{{$chirp['author']}}</div>
-                        <div class="mt-1">{{$chirp['message']}}</div>
-                        <div class="text-sm text-gray-500 mt-2">{{$chirp['time']}}</div>
+                        <div class="font-bold">{{$chirp->user ? $chirp->user->name : "Anonymous"}}</div>
+                        <div class="mt-1">{{$chirp->message}}</div>
+                        <div class="text-sm text-gray-500 mt-2">{{
+                            $chirp->created_at->diffForHumans()}}</div>
                     </div>
                 </div>
             </div>
-        @endforeach
+        @empty
+            <p class="text-gray-500">No chirps here. Be the first one!</p>
+        @endforelse
     </div>
 </x-layout>
